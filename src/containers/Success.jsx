f41ef432971dd99dbe1a290f36412d81736bf4b8
Bryan Thomas <bryan.thomas25000@icloud.com>
2021-05-17 00:11:32 -0500

    handled success exception

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -7,12 +7,17 @@ import useGoogleAddress from '../hooks/useGoogleAddress';
 const Success = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
-  const location = useGoogleAddress(buyer[0].address);
+  let address = '';
+  if (typeof buyer[0] === 'undefined')
+    address = `1600+Amphitheatre+Parkway,+Mountain+View,+CA`;
+  else address = buyer[0].address;
+  const location = useGoogleAddress(address);
   return (
     <div className="Sucess">
       <div className="Sucess-content">
         <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias a tu direccion</span>
+
         <div className="Sucess-map">
           <Map data={location} />
         </div>
